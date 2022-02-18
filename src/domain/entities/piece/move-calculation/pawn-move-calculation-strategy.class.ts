29dd6bd3df122ee2ce1cd7363cebada3bc41d4a4
Barkın <barkin.sagin@cubicl.com>
2022-02-18 17:39:24 +0300

    gave pawns the ability to move 2 squares forward in their first movements

--- src/domain/entities/piece/move-calculation/pawn-move-calculation-strategy.class.ts
+++ src/domain/entities/piece/move-calculation/pawn-move-calculation-strategy.class.ts
@@ -17,11 +17,22 @@ export class PawnMoveCalculationStrategy extends MoveCalculationStrategy {
 		const pieceOwner = piece.color === this._dataStore.getColor("player") ? "player" : "opponent";
 		const squarePieceIsOn = this._dataStore.getSquareById(piece.squareId);
 
+		// Check if pawn can move forward
 		const squareInTop = this._boardNavigator.getFirstSquareInDirection(squarePieceIsOn, "top", pieceOwner);
 		if (squareInTop && !this._dataStore.getPieceOnSquare(squareInTop)) {
 			possibleMoves.push(squareInTop);
 		}
 
+		// Pawn can move 2 squares forward during its first move. Check if it can move forward 2nd time
+		if (squareInTop && this.isPawnOnStartingLocation(piece)) {
+			const squareIn2ndTop = this._boardNavigator.getFirstSquareInDirection(squareInTop, "top", pieceOwner);
+			if (squareIn2ndTop && !this._dataStore.getPieceOnSquare(squareIn2ndTop)) {
+				possibleMoves.push(squareIn2ndTop);
+			}
+		}
+
+		// Pawn can capture opponent pieces if they are on pawn's top left or top right.
+		// ------------------------------------------------------------------------------------------------------
 		const squareInTopLeft = this._boardNavigator.getFirstSquareInDirection(squarePieceIsOn, "top_left", pieceOwner);
 		if (squareInTopLeft) {
 			const targetPiece = this._dataStore.getPieceOnSquare(squareInTopLeft);
@@ -41,7 +52,18 @@ export class PawnMoveCalculationStrategy extends MoveCalculationStrategy {
 				possibleMoves.push(squareInTopRight);
 			}
 		}
-
+		//----------------------------------------------------------------------------------------------------------
 		return possibleMoves;
+	}
+
+	isPawnOnStartingLocation(piece: Piece): boolean {
+		if (!piece.squareId) throw new Error("piece is not on board");
+
+		const pieceOwner = piece.color === this._dataStore.getColor("player") ? "player" : "opponent";
+		const squarePieceIsOn = this._dataStore.getSquareById(piece.squareId);
+
+		const theSquareBehind = this._boardNavigator.getFirstSquareInDirection(squarePieceIsOn, "bottom", pieceOwner)!;
+
+		return !this._boardNavigator.getFirstSquareInDirection(theSquareBehind, "bottom", pieceOwner);
 	}
 }
