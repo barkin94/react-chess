67388bcc8953cd52f12fcf7cb5511926e6484560
Barkın <barkin.sagin@cubicl.com>
2022-10-12 19:47:32 +0300

    refactor in QueenMoveCalculationStrategy class

--- src/domain/entities/piece/move-calculation/queen-move-calculation-strategy.class.ts
+++ src/domain/entities/piece/move-calculation/queen-move-calculation-strategy.class.ts
@@ -1,35 +1,36 @@
 import { inject, injectable } from "inversify";
-import { BoardNavigator, Direction } from "./board-navigator.class";
-import { Square } from "../../board/square.class";
-import { MoveCalculationStrategy } from "./move-calculation-strategy.abstract";
-import { Piece } from "../piece.class";
 import { Side } from "../../../shared/types/side.type";
+import { Square } from "../../board/square.class";
+import { Piece } from "../piece.class";
+import { BoardNavigator, Direction } from "./board-navigator.class";
+import { MoveCalculationStrategy } from "./move-calculation-strategy.abstract";
 
 @injectable()
 export class QueenMoveCalculationStrategy extends MoveCalculationStrategy {
 	@inject(BoardNavigator)
 	private _boardNavigator!: BoardNavigator;
 
+	private directionsPieceCanMove: Direction[] = [
+		"bottom",
+		"bottom_left",
+		"bottom_right",
+		"left",
+		"right",
+		"top",
+		"top_left",
+		"top_right",
+	];
+
 	getPossibleMoves(piece: Piece): Square[] {
-		if (!piece.squareId) return [];
+		if (!piece.squareId)
+			throw new Error("piece is not on the board");
 
 		const square = this._dataStore.getSquareById(piece.squareId);
 
 		const possibleMoves: Square[] = [];
 		const side: Side = piece.color === this._dataStore.getColor("player") ? "player" : "opponent";
 
-		const directions: Direction[] = [
-			"bottom",
-			"bottom_left",
-			"bottom_right",
-			"left",
-			"right",
-			"top",
-			"top_left",
-			"top_right",
-		];
-
-		directions.forEach((direction) => {
+		this.directionsPieceCanMove.forEach((direction) => {
 			const squaresInDirection = this._boardNavigator.getAllSquaresInDirection(square, direction, side);
 			possibleMoves.push(...this.endSequenceWhenEncounteredPiece(squaresInDirection, piece.color));
 		});
